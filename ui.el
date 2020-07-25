b56875970250167cdd2fc7d09b213067fe880380
neeasade <neeasade@gmail.com>
2020-07-25 11:07:33 -0500

    some initial ui stuff -- need to grok tabulated list mode

---
+++ ui.el
@@ -0,0 +1,36 @@
+;; a transient example?:
+;; https://gist.github.com/abrochard/dd610fc4673593b7cbce7a0176d897de
+
+;; huuuuuuuuuge thanks to @abrochard
+
+;; number is percent?
+(let ((columns [("Col1" 50) ("Col2" 50)])
+       (rows (list
+               '(nil ["row1" "value1"])
+               '(nil ["row2" "value2"])
+               '(nil ["row3" "value3"]))))
+  (switch-to-buffer "*temp*")
+  (setq tabulated-list-format columns)
+  (setq tabulated-list-entries rows)
+  (tabulated-list-init-header)
+  (tabulated-list-print))
+
+(define-derived-mode livingstone-mode tabulated-list-mode "Livingstone"
+  "Kubernetes mode"
+  (let ((columns
+          ;; todo
+          ;; [("Pod" 100)]
+          )
+         (rows
+           ;; todo
+           ;; '("this" "is" "test")
+           ))
+    (setq tabulated-list-format columns)
+    (setq tabulated-list-entries rows)
+    (tabulated-list-init-header)
+    (tabulated-list-print)))
+
+(defun livingstone ()
+  (interactive)
+  (switch-to-buffer "*livingstone*")
+  (livingstone-mode))
