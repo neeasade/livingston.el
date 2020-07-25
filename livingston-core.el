a506aaabaddd6b0e1b43d820f29debcf252b5d85
Cerek Hillen <chillen@lyft.com>
2020-07-25 12:02:25 -0400

    ???

---
+++ livingston-core.el
@@ -0,0 +1,11 @@
+;; get the sugar
+(progn
+  (use-package s)    ;; string
+  (use-package f)    ;; file
+  (use-package ht)   ;; hash table
+  (use-package dash) ;; list
+  )
+
+(defun ffl/roll-die (&optional times)
+  (* (or times 1)
+    (+ 1 (random 6))))
